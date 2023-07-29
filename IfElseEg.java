dc2a9b12e01aee65634c186d7f88f7fa7d4223c9
AdvikaPuthran <138208645+AdvikaPuthran@users.noreply.github.com>
2023-07-29 16:37:52 +0530

    Add files via upload

---
+++ IfElseEg.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; //import scanner class
+
+class IfElseEg
+{
+	static void calc_passfail(int marks)
+	{
+		if(marks>=35)
+		{
+			System.out.println("You are Pass... Congrats"); //print if true
+		}
+		else
+		{
+			System.out.println("You are Fail.... Better luck next time"); //print if else
+		}
+	}
+
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in); //object of Scanner
+		System.out.println("Enter marks");
+		int marks=sc.nextInt(); //read user input
+
+		calc_passfail(marks); //calling static method
+	}
+}
---
+++ VariableExample.java
@@ -0,0 +1,25 @@
+class VariableExample 
+{
+	static String name="Advika P"; //static variable
+
+	//non static method
+	void display()
+	 {
+		//local variables
+		char my= 'm';
+		int num= 89;
+		System.out.println(my + " " + num);
+	}
+	 long phone= 767809045; //instance variable
+
+	//entry point
+	public static void main(String args[])
+	{
+		//creating an object/instance
+		VariableExample obj= new VariableExample();
+
+		obj.display(); //calling method
+		System.out.println(obj.phone); //accessing instance variable
+		System.out.println(VariableExample.name); //accessing static variable
+	}
+}
